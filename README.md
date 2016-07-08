97480536af3b420955893da40fa6303ce2b8466b
internelp <gaof.d@foxmail.com>
2016-07-08 15:10:48 +0800

    nginx状态页取值

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # check-nginx-status
 监控nginx状态
+
+	需要预先配置Nginx的监控页面
+```
+location /nginx_status {
+        stub_status     on;
+        access_log      off;
+        allow           127.0.0.1;
+        deny all;
+}
+```
+
+##使用方法
+第一个参数输入需要检测的状态
+
+	脚本名	参数
+	check-nginx-status.sh ping
+	check-nginx-status.sh active
---
+++ check-nginx-status.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+# 创建：高峰
+# 时间：2016-07-08
+
+HOST="127.0.0.1"
+PORT="11010"
+
+
+function ping {
+	# 检测Nginx Worker Process进程数量
+    ps -ef|grep "nginx: worker process"|grep -v grep|wc -l
+}
+
+
+# 检测nginx性能
+function active {
+    /usr/bin/curl "http://$HOST:$PORT/nginx_status/" 2>/dev/null | grep 'Active' | awk '{print $NF}'
+}
+function reading {
+    /usr/bin/curl "http://$HOST:$PORT/nginx_status/" 2>/dev/null | grep 'Reading' | awk '{print $2}'
+}
+function writing {
+    /usr/bin/curl "http://$HOST:$PORT/nginx_status/" 2>/dev/null | grep 'Writing' | awk '{print $4}'
+}
+function waiting {
+    /usr/bin/curl "http://$HOST:$PORT/nginx_status/" 2>/dev/null | grep 'Waiting' | awk '{print $6}'
+}
+function accepts {
+    /usr/bin/curl "http://$HOST:$PORT/nginx_status/" 2>/dev/null | awk NR==3 | awk '{print $1}'
+}
+function handled {
+    /usr/bin/curl "http://$HOST:$PORT/nginx_status/" 2>/dev/null | awk NR==3 | awk '{print $2}'
+}
+function requests {
+    /usr/bin/curl "http://$HOST:$PORT/nginx_status/" 2>/dev/null | awk NR==3 | awk '{print $3}'
+}
+
+
+if [[ -z $1 ]]; then
+	# 如果第一个参数没有，就把所有都打印出来。
+	 /usr/bin/curl "http://$HOST:$PORT/nginx_status/"
+else
+	$1
+fi
