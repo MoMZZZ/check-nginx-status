9154c3ce9fbfb2ccffba2d98e9dacb71699b9803
internelp <gaof.d@foxmail.com>
2016-07-08 15:12:55 +0800

    nginx状态页取值

--- check-nginx-status.sh
+++ check-nginx-status.sh
@@ -14,31 +14,31 @@ function ping {
 
 # 检测nginx性能
 function active {
-    /usr/bin/curl "http://$HOST:$PORT/nginx_status/" 2>/dev/null | grep 'Active' | awk '{print $NF}'
+    /usr/bin/curl "http://$HOST:$PORT/nginx_status" 2>/dev/null | grep 'Active' | awk '{print $NF}'
 }
 function reading {
-    /usr/bin/curl "http://$HOST:$PORT/nginx_status/" 2>/dev/null | grep 'Reading' | awk '{print $2}'
+    /usr/bin/curl "http://$HOST:$PORT/nginx_status" 2>/dev/null | grep 'Reading' | awk '{print $2}'
 }
 function writing {
-    /usr/bin/curl "http://$HOST:$PORT/nginx_status/" 2>/dev/null | grep 'Writing' | awk '{print $4}'
+    /usr/bin/curl "http://$HOST:$PORT/nginx_status" 2>/dev/null | grep 'Writing' | awk '{print $4}'
 }
 function waiting {
-    /usr/bin/curl "http://$HOST:$PORT/nginx_status/" 2>/dev/null | grep 'Waiting' | awk '{print $6}'
+    /usr/bin/curl "http://$HOST:$PORT/nginx_status" 2>/dev/null | grep 'Waiting' | awk '{print $6}'
 }
 function accepts {
-    /usr/bin/curl "http://$HOST:$PORT/nginx_status/" 2>/dev/null | awk NR==3 | awk '{print $1}'
+    /usr/bin/curl "http://$HOST:$PORT/nginx_status" 2>/dev/null | awk NR==3 | awk '{print $1}'
 }
 function handled {
-    /usr/bin/curl "http://$HOST:$PORT/nginx_status/" 2>/dev/null | awk NR==3 | awk '{print $2}'
+    /usr/bin/curl "http://$HOST:$PORT/nginx_status" 2>/dev/null | awk NR==3 | awk '{print $2}'
 }
 function requests {
-    /usr/bin/curl "http://$HOST:$PORT/nginx_status/" 2>/dev/null | awk NR==3 | awk '{print $3}'
+    /usr/bin/curl "http://$HOST:$PORT/nginx_status" 2>/dev/null | awk NR==3 | awk '{print $3}'
 }
 
 
 if [[ -z $1 ]]; then
 	# 如果第一个参数没有，就把所有都打印出来。
-	 /usr/bin/curl "http://$HOST:$PORT/nginx_status/"
+	 /usr/bin/curl "http://$HOST:$PORT/nginx_status"
 else
 	$1
 fi
